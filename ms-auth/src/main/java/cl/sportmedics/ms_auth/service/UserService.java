b36adcde99d49743ba2481164aea0e87dafe076a
zhephirot <an.poncev@duocuc.cl>
2026-05-17 15:26:24 -0400

    rename
    rename del service

--- ms-auth/src/main/java/cl/sportmedics/ms_auth/service/UserService.java
+++ ms-auth/src/main/java/cl/sportmedics/ms_auth/service/UserService.java
@@ -1,43 +1,11 @@
 package cl.sportmedics.ms_auth.service;
 
 import com.sportmedics.auth.entity.User;
-import com.sportmedics.auth.repository.UserRepository;
-import com.sportmedics.auth.service.IUserService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service
-@Slf4j
-public class UserServiceImpl implements IuserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public List<User> findAll() {
-        log.info("Obteniendo lista de todos los usuarios");
-        return userRepository.findAll();
-    }
-
-    @Override
-    public User save(User user) {
-        log.info("Guardando nuevo usuario: {}", user.getUsername());
-        return userRepository.save(user);
-    }
-
-    @Override
-    public User findById(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> {
-            log.error("Usuario con ID {} no encontrado", id);
-            return new RuntimeException("Usuario no encontrado");
-        });
-    }
-
-    @Override
-    public void delete(Long id) {
-        log.warn("Eliminando usuario con ID: {}", id);
-        userRepository.deleteById(id);
-    }
+public interface IuserService {
+    List<User> findAll();
+    User save(User user);
+    User findById(Long id);
+    void delete(Long id);
 }
