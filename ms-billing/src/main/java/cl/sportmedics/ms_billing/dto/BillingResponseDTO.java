9bb7bed426d1693a833b44fe6d978a9516cf14e9
lich73 <d.aedo.esc3@gmail.com>
2026-05-17 16:27:06 -0400

    Creación del dto
    Request
    Response

--- ms-billing/src/main/java/cl/sportmedics/ms_billing/dto/BillingResponseDTO.java
+++ ms-billing/src/main/java/cl/sportmedics/ms_billing/dto/BillingResponseDTO.java
@@ -1,5 +1,14 @@
 package cl.sportmedics.ms_billing.dto;
 
-public class BillingResponseDTO {
+import lombok.Data;
 
+@Data
+public class BillingResponseDTO {
+    private Long id;
+    private String rut;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Boolean active;
 }
