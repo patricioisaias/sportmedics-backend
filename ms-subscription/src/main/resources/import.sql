3b054012f0a65ef69ad7c80b3f0b65d5863cc34a
lich73 <d.aedo.esc3@gmail.com>
2026-05-18 18:58:02 -0400

    ms-subsciption BD fix

--- ms-subscription/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO subscriptions (name, price, duration_months, active) VALUES ('Plan Mensual Básico', 25000, 1, true);
-INSERT INTO subscriptions (name, price, duration_months, active) VALUES ('Plan Trimestral Pro', 65000, 3, true);
-INSERT INTO subscriptions (name, price, duration_months, active) VALUES ('Plan Anual VIP', 200000, 12, true);
