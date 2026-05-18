fc1731cb7cb7791784c14fc8bb7122da53a7bf74
lich73 <d.aedo.esc3@gmail.com>
2026-05-18 00:27:43 -0400

    update acces bd

--- ms-access/src/main/resources/data.sql
+++ ms-access/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT IGNORE INTO accesses (member_id, access_date_time, granted, denial_reason) VALUES (1, '2026-05-17 08:30:00', true, NULL);
 INSERT IGNORE INTO accesses (member_id, access_date_time, granted, denial_reason) VALUES (2, '2026-05-17 09:15:00', false, 'MEMBRESIA EXPIRED O MOROSO');
-INSERT IGNORE INTO accesses (member_id, access_date_time, granted, denial_reason) VALUES (3, '2026-05-17 10:00:00', true, NULL);
+INSERT IGNORE INTO accesses(member_id, access_date_time, granted, denial_reason) VALUES (3, '2026-05-17 10:00:00', true, NULL);
