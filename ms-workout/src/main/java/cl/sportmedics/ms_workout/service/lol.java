d9859742f576202575a229201526746f579e28d9
Patricio Céspedes <patricio.isaiascv@gmail.com>
2026-05-17 21:57:58 -0400

    arreglo

---
+++ ms-workout/src/main/java/cl/sportmedics/ms_workout/service/lol.java
@@ -0,0 +1,15 @@
+package cl.sportmedics.ms_workout.service;
+
+import cl.sportmedics.ms_workout.dto.WorkoutRequestDTO;
+import cl.sportmedics.ms_workout.dto.WorkoutResponseDTO;
+import java.util.List;
+
+public interface WorkoutService {
+    WorkoutResponseDTO create(WorkoutRequestDTO dto);
+
+    List<WorkoutResponseDTO> getAll();
+
+    WorkoutResponseDTO getById(Long id);
+
+    void delete(Long id);
+}
