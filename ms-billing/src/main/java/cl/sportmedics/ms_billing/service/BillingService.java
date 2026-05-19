2d71f41913c2e880117d56b51bca5aff6747bf58
Patricio Céspedes <patricio.isaiascv@gmail.com>
2026-05-18 21:07:53 -0400

    añade openfeign a ms-billing

--- ms-billing/src/main/java/cl/sportmedics/ms_billing/service/BillingService.java
+++ ms-billing/src/main/java/cl/sportmedics/ms_billing/service/BillingService.java
@@ -1,15 +1,23 @@
 package cl.sportmedics.ms_billing.service;
 
-
 import cl.sportmedics.ms_billing.dto.BillingRequestDTO;
 import cl.sportmedics.ms_billing.dto.BillingResponseDTO;
 import java.util.List;
 
 public interface BillingService {
     BillingResponseDTO create(BillingRequestDTO dto);
+
     List<BillingResponseDTO> getAll();
+
     BillingResponseDTO getById(Long id);
+
     BillingResponseDTO update(Long id, BillingRequestDTO dto);
+
     void delete(Long id);
+
     void processPayment(Long invoiceId, Double amount, String method, String ref);
+
+    boolean checkDebtStatus(Long memberId);
+
+    void initializeAccount(Long memberId);
 }
