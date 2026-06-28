8fa61807e7cd0782144cb4d4af6f0b9ed0a91830
lich73 <d.aedo.esc3@gmail.com>
2026-06-28 19:17:26 -0400

    swagger update fix 2.1

--- sportmedics-maven/ms-inventory/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO inventory_items (name, category, quantity, location, active) VALUES ('Cinta de Correr Pro', 'MACHINES', 5, 'Zona Cardio', true);
-INSERT IGNORE INTO inventory_items (name, category, quantity, location, active) VALUES ('Mancuernas 10kg', 'WEIGHTS', 12, 'Zona Peso Libre', true);
-INSERT IGNORE INTO inventory_items (name, category, quantity, location, active) VALUES ('Colchoneta Yoga', 'ACCESSORIES', 20, 'Sala Clases Grupales', true);
